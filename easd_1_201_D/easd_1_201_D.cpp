d898b7e22f632b10f35520613441ece334ff564e
MusthafaLuthfi76 <musthafa.luthfi76@gmail.com>
2023-04-07 09:16:27 +0700

    Jawaban No.1

--- easd_1_201_D/easd_1_201_D.cpp
+++ easd_1_201_D/easd_1_201_D.cpp
@@ -1,4 +1,6 @@
-// 1. 
+// 1. Ilmu komputer adalah bidang studi yang berhubungan dengan pemecahan berbagai masalah dengan menggunakan komputer. 
+//    Untuk mengatasi masalah yang diberikan dengan menggunakan komputer, kita perlu merancang algoritma untuk itu. algoritma memungkinkan kita 
+//    untuk merancang proses yang sistematis dan efektif untuk menyelesaikan masalah.
 
 
 
