7ada9f43b99d46ae61bf33606740ffb691dc47f0
MusthafaLuthfi76 <musthafa.luthfi76@gmail.com>
2023-04-07 09:37:08 +0700

    Jawaban no. 4

--- easd_1_201_D/easd_1_201_D.cpp
+++ easd_1_201_D/easd_1_201_D.cpp
@@ -1,10 +1,12 @@
-// 1. Ilmu komputer adalah bidang studi yang berhubungan dengan pemecahan berbagai masalah dengan menggunakan komputer. 
+//1. Ilmu komputer adalah bidang studi yang berhubungan dengan pemecahan berbagai masalah dengan menggunakan komputer. 
 //    Untuk mengatasi masalah yang diberikan dengan menggunakan komputer, kita perlu merancang algoritma untuk itu. algoritma memungkinkan kita 
 //    untuk merancang proses yang sistematis dan efektif untuk menyelesaikan masalah.
 
 //2. Data struktur diklasifikasikan jadi 2 : Static, & Dynamic
 
 //3. Kecepatan mesin, Compiler, Operating System, Bahasa Pemrograman, Ukuran dari Output
+
+//4. Insertion Sort, Karena waktu terbaiknya O(n) kalau sudah hampir urut. Dan algoritma ini sederhana dan mudah diimplementasikan
 
 
 
