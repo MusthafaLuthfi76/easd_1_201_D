2d7325b3356fd77d4d230978f1946294e4f3fcc0
MusthafaLuthfi76 <musthafa.luthfi76@gmail.com>
2023-04-07 10:23:11 +0700

    Ngerapiin

--- easd_1_201_D/easd_1_201_D.cpp
+++ easd_1_201_D/easd_1_201_D.cpp
@@ -16,20 +16,20 @@
 #include <iostream>
 using namespace std;
 
-int luthfi[21];          //Deklarasi array a dengan ukuran 20
-int n;              //Deklarasi variabel n untuk menyimpan banyaknya elemen pada array
+int luthfi[21];             //Deklarasi array luthfi dengan ukuran 20
+int n;                      //Deklarasi variabel n untuk menyimpan banyaknya elemen pada array
 
-void input()        //Procedure untuk input
+void input()                //Procedure untuk input
 {
-    while (true)    //Looping
+    while (true)            //Looping
     {
-        cout << "Masukkan banyaknya elemen pada array : "; //Output ke layar
-        cin >> n;       //Input dari pengguna
-        if (n <= 20)    //Jika n kurang dari atau sama dengan 20
-            break;      //Keluar dari loop
+        cout << "Masukkan banyaknya elemen pada array : ";      //Output ke layar
+        cin >> n;                                               //Input dari pengguna
+        if (n <= 20)                                            //Jika n kurang dari atau sama dengan 20
+            break;                                              //Keluar dari loop
         else
-        {               //Jika n lebih dari 20
-            cout << "\nArray dapat mempunyai maksimal 20 elemen.\n";   //Output ke layar 
+        {                                                       //Jika n lebih dari 20
+            cout << "\nArray dapat mempunyai maksimal 20 elemen.\n";        //Output ke layar 
         }
     }
     cout << endl;                               //Output Baris Kosong
@@ -37,9 +37,9 @@ void input()        //Procedure untuk input
     cout << "Masukkan Elemen Array" << endl;    //Output ke layar
     cout << "====================" << endl;     //Output ke layar
 
-    for (int i = 0; i < n; i++) {			//Looping dengan i dimulai dari 0 hingga n-1
-        cout << "Data Ke-" << (i + 1) << ": ";	//Output ke layar
-        cin >> luthfi[i];					//Input dari pengguna
+    for (int i = 0; i < n; i++) {			        //Looping dengan i dimulai dari 0 hingga n-1
+        cout << "Data Ke-" << (i + 1) << ": ";	    //Output ke layar
+        cin >> luthfi[i];					        //Input dari pengguna
     }
 }
 
@@ -67,8 +67,8 @@ void display() {                            //Procedure display
     cout << "====================" << endl;                 //Output ke layar
     cout << "Element Array yang telah tersusun" << endl;    //Output ke layar
     cout << "====================" << endl;                 //Output ke layar
-    for (int j = 0; j < n; j++) {           //Looping dengan j dimulai dari 0 hingga n-1
-        cout << luthfi[j] << endl;               //Output ke layar
+    for (int j = 0; j < n; j++) {               //Looping dengan j dimulai dari 0 hingga n-1
+        cout << luthfi[j] << endl;              //Output ke layar
     }
     cout << endl;                           //Output baris kosong
 }
