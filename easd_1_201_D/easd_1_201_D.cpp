20b92c46aaf0775e782bdae4579cf221e51d4c95
MusthafaLuthfi76 <musthafa.luthfi76@gmail.com>
2023-04-07 09:52:54 +0700

    Membuat procedure input

--- easd_1_201_D/easd_1_201_D.cpp
+++ easd_1_201_D/easd_1_201_D.cpp
@@ -11,11 +11,38 @@
 //5. Quadratic : Bubble Sort, Insertion Sort, Selection Sort
 //   LogLinear : Merge Sort, Quick Sort
 
-
-
-
+//6. Program Selection Sort
 
 #include <iostream>
+using namespace std;
+
+int luthfi[20];          //Deklarasi array a dengan ukuran 20
+int n;              //Deklarasi variabel n untuk menyimpan banyaknya elemen pada array
+
+void input()        //Procedure untuk input
+{
+    while (true)    //Looping
+    {
+        cout << "Masukkan banyaknya elemen pada array : "; //Output ke layar
+        cin >> n;       //Input dari pengguna
+        if (n <= 20)    //Jika n kurang dari atau sama dengan 20
+            break;      //Keluar dari loop
+        else
+        {               //Jika n lebih dari 20
+            cout << "\nArray dapat mempunyai maksimal 20 elemen.\n";   //Output ke layar 
+        }
+    }
+    cout << endl;                               //Output Baris Kosong
+    cout << "====================" << endl;     //Output ke layar
+    cout << "Masukkan Elemen Array" << endl;    //Output ke layar
+    cout << "====================" << endl;     //Output ke layar
+
+    for (int i = 0; i < n; i++) {			//Looping dengan i dimulai dari 0 hingga n-1
+        cout << "Data Ke-" << (i + 1) << ": ";	//Output ke layar
+        cin >> a[i];					//Input dari pengguna
+    }
+}
+
 
 int main()
 {
