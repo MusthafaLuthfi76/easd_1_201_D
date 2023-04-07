5c73633db4ec64de0aad35d579c660a371895de5
MusthafaLuthfi76 <musthafa.luthfi76@gmail.com>
2023-04-07 09:54:22 +0700

    Mengganti jumlah array

--- easd_1_201_D/easd_1_201_D.cpp
+++ easd_1_201_D/easd_1_201_D.cpp
@@ -16,7 +16,7 @@
 #include <iostream>
 using namespace std;
 
-int luthfi[20];          //Deklarasi array a dengan ukuran 20
+int luthfi[21];          //Deklarasi array a dengan ukuran 20
 int n;              //Deklarasi variabel n untuk menyimpan banyaknya elemen pada array
 
 void input()        //Procedure untuk input
